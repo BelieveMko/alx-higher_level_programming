cca7bbe063a9065ea166f708714c6c7809e3b535
Believe Mukomberanwa <believemuko1@gmail.com>
2024-03-23 21:45:51 +0200

    print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
+for a in range(0, 10):
+    for b in range(a + 1, 10):
+        if a == 8 and b == 9:
             print('89')
         else:
-            print('{}{}, '.format(x, y), end='')
+            print('{}{}, '.format(a, b), end='')
